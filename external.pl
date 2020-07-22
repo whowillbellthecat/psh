bd9cd7568b7b2ae9b72d43812af551b54c37aa79
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-07-22 18:12:46 -0500

    add op/3 entry for file

--- external.pl
+++ external.pl
@@ -1,5 +1,6 @@
 :- op(100, fx, vi).
 :- op(100, fx, make).
+:- op(100, fx, file).
 
 % todo : vi/2 should spawn vi with a temporary file, and then unify the 2nd var with contents of that file.
 %    by default as a list of lines but possibly consider vi/3 with an early param specifying the mode/interpretation
