8554b378e6b2a24c36fe869e20e371f65b8df910
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-07-24 18:24:31 -0500

    revise syntax ((<-) precedence and change $VAR<-t pattern to atom<-t)

--- pipe.pl
+++ pipe.pl
@@ -1,4 +1,4 @@
-:- op(950, xfx, <-).
+:- op(951, xfx, <-).
 :- op(650, yfx, '<>').
 :- op(950, xfx, <=).
 :- op(950, xfy, <--).
@@ -8,7 +8,7 @@ $X :- puts <-- g_read(X).
 $(X,Y) :- g_read(X,Y).
 
 X <- P :- var(X), call(P,X).
-$X <- P :- atom(X), call(P,Y), g_assign(X,Y).
+X <- P :- atom(X), call(P,Y), g_assign(X,Y).
 
 Z <-- P :- \+ functor(P,(<>),_), var(Z), call(P,Z).
 Z <-- P :- functor(P,(<>),_), var(Z), ([V|Vs] <= P), call(V,X), maplist(call,Vs,[X|Ts],Y), reverse(Y,[Z|Zs]), reverse(Zs,Ts).
