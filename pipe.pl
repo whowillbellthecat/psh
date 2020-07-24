f1edebbdaf0194132f1170d081d98153cf70f22d
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-07-24 17:47:36 -0500

    add (<--)/3 and set (<--) to be right-associative

--- pipe.pl
+++ pipe.pl
@@ -1,7 +1,7 @@
 :- op(950, xfx, <-).
 :- op(650, yfx, '<>').
 :- op(950, xfx, <=).
-:- op(950, xfx, <--).
+:- op(950, xfy, <--).
 :- op(100, fx, $).
 
 $X :- puts <-- g_read(X).
@@ -13,6 +13,8 @@ $X <- P :- atom(X), call(P,Y), g_assign(X,Y).
 Z <-- P :- \+ functor(P,(<>),_), var(Z), call(P,Z).
 Z <-- P :- functor(P,(<>),_), var(Z), ([V|Vs] <= P), call(V,X), maplist(call,Vs,[X|Ts],Y), reverse(Y,[Z|Zs]), reverse(Zs,Ts).
 Z <-- P :- callable(Z), (R <-- P), call(Z,R), !. %may not be best place for cut?
+
+'<--'(V,P,X) :- callable(V), Z <-- P, call(V,Z,X).
 
 V <= (P<>Q<>[R|Rs]) :- V <= (P<>[Q,R|Rs]).
 V <= (P<>Q<>R) :- V <= (P<>[Q|[R]]).
