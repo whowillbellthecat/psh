f79b90367965c65d6d94d2dd7d93735326856431
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-08-07 23:08:13 -0500

    define rot/5, swap/4, and drop/3

--- comb.pl
+++ comb.pl
@@ -1,4 +1,7 @@
 rot(P,X,Y,Z) :- call(P,Z,X,Y).
+rot(P,X,Y,Z,A) :- call(P,Z,X,Y,A).
 swap(P,X,Y) :- call(P,Y,X).
+swap(P,X,Y,Z) :- call(P,Y,X,Z).
 dup(P,X,X) :- call(P,X).
 drop(P,X) :- call(P,X,_).
+drop(P,X,Y) :- call(P,X,Y,_).
