799aeeeaac8d760eb9d596373fd50447a0bf9aea
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-08-07 19:11:07 -0500

    add (\+)/2

--- misc.pl
+++ misc.pl
@@ -2,3 +2,6 @@
 %match clause with predicate name, e.g. `filter(cf(pred/2), Domain, Matching)`
 cf(H/N,(H0:-_)) :- functor(H0,H,N), !.
 cf(H,(H0:-_)) :- atom(H),functor(H0,H,_).
+
+%defined such that call((\+ p),Y) is interpreted as \+ p(Y).
+\+(X,Y) :- \+ call(X,Y).
