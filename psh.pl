7cd65e026d5211129c68e14301924a75c06d608f
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-08-07 23:09:20 -0500

    make `edit +P/N' start editing at the line where P/N is defined

--- psh.pl
+++ psh.pl
@@ -10,7 +10,7 @@ via(X,F,R) :- R <-- fl F <> filter(cf(X)).
 X via F :- maplist(portray_clause) <-- X via F.
 
 edit T :- atom(T), atom_concat(T,'.pl',F), vi F.
-edit(+P/N) :- atom(P), where(P/N, F), vi F.
+edit(+P/N) :- atom(P), where(P/N, F), whichline(P/N,L), vi(L,F).
 %edit(X via F) :-  % for this to work I'll need a way to reinsert edited content into the file.
 	% what I need is a bidirectional mapping to/from the concise notation I prefer to use so
 	% that I can map portray_clause<->concise notation. Though without care comments will end up being removed.
