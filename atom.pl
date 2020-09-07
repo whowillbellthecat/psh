bc3675e90eb07ec8da3fc8b2d8ddbd30fede262a
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-09-07 15:27:38 -0500

    add ++

--- atom.pl
+++ atom.pl
@@ -1,2 +1,9 @@
+:- op(450,xfy,++).
+
 endswith(E,A) :- atom_concat(_,E,A).
 startswith(S,A) :- atom_concat(S,_,A).
+
+++(X,Y,R) :- atom(X), atom(Y), atom_concat(X,Y,R).
+++(X,Y,R) :- callable(X), call(X,X0), ++(X0,Y,R).
+++(X,Y,R) :- callable(Y), call(Y,Y0), ++(X,Y0,R).
+X++Y :- X++Y <> (=).
