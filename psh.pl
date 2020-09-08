5db7a492f1c8f50cdf491b8b131b535b196d4f9c
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-09-08 13:28:54 -0500

    remove commented out edit/1 clause

--- psh.pl
+++ psh.pl
@@ -11,9 +11,6 @@ X via F :- maplist(portray_clause) <-- X via F.
 
 edit T :- atom(T), atom_concat(T,'.pl',F), vi F.
 edit(+P/N) :- atom(P), where(P/N, F), whichline(P/N,L), vi(L,F).
-%edit(X via F) :-  % for this to work I'll need a way to reinsert edited content into the file.
-	% what I need is a bidirectional mapping to/from the concise notation I prefer to use so
-	% that I can map portray_clause<->concise notation. Though without care comments will end up being removed.
 
 load_pshrc :- file_exists('~/.pshrc'), consult('~/.pshrc').
 
