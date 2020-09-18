a65ad1ad3a53265f5156e8d83fa2bf426bc81f06
whowillbellthecat <everythingisbotnet@protonmail.com>
2020-09-18 01:18:44 -0500

    define (++.)

--- atom.pl
+++ atom.pl
@@ -1,4 +1,5 @@
 :- op(399,xfy,++).
+:- op(399,xfy,++.).
 
 endswith(E,A) :- atom_concat(_,E,A).
 startswith(S,A) :- atom_concat(S,_,A).
@@ -8,3 +9,6 @@ atom_resolve(X,Y) :- callable(X), call(X,Y), atom(Y).
 
 ++(X,Y,R) :- atom_resolve(X,X0), atom_resolve(Y,Y0), atom_concat(X0,Y0,R).
 X++Y :- X++Y <> (=).
+
+++.(X,Y,R) :- R <- X++'.'++Y.
+X ++. Y :- X ++. Y <> (=).
