32eee1bbf5ba6b4c79a723baa1bd312063f218c0
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-03-05 15:23:00 -0600

    rename (++.)/2 to dot/2

--- atom.pl
+++ atom.pl
@@ -1,5 +1,5 @@
 :- op(399,xfy,++).
-:- op(399,xfy,++.).
+:- op(399,xfy,dot).
 
 endswith(E,A) :- atom_concat(_,E,A).
 startswith(S,A) :- atom_concat(S,_,A).
@@ -10,5 +10,5 @@ atom_resolve(X,Y) :- callable(X), call(X,Y), atom(Y).
 ++(X,Y,R) :- atom_resolve(X,X0), atom_resolve(Y,Y0), atom_concat(X0,Y0,R).
 X++Y :- X++Y <> (=).
 
-++.(X,Y,R) :- R <- X++'.'++Y.
-X ++. Y :- X ++. Y <> (=).
+dot(X,Y,R) :- R <- X++'.'++Y.
+X dot Y :- X dot Y <> (=).
