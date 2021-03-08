fda554a6d669d7eb0a5fdbbf83dbe6d990b096fd
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-03-08 09:49:52 -0600

    extend atom_resolve/2 to support numbers

--- atom.pl
+++ atom.pl
@@ -5,6 +5,7 @@ endswith(E,A) :- atom_concat(_,E,A).
 startswith(S,A) :- atom_concat(S,_,A).
 
 atom_resolve(X,X) :- atom(X), !.
+atom_resolve(X,Y) :- number(X), !, number_atom(X,Y).
 atom_resolve(X,Y) :- callable(X), call(X,Y), atom(Y).
 
 ++(X,Y,R) :- atom_resolve(X,X0), atom_resolve(Y,Y0), atom_concat(X0,Y0,R).
