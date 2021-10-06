a475759c1280728559bc3329b8d9c1c53a0d33ad
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-10-06 12:11:17 -0500

    update cf/2 to also match (=>)/2 syntax

--- misc.pl
+++ misc.pl
@@ -6,7 +6,9 @@ P or Q :- call(P) ; call(Q).
 
 %match clause with predicate name, e.g. `filter(cf(pred/2), Domain, Matching)`
 cf(H/N,(H0:-_)) :- functor(H0,H,N), !.
-cf(H,(H0:-_)) :- atom(H),functor(H0,H,_).
+cf(H/N,(H0=>_)) :- functor(H0,H,N), !.
+cf(H,(H0:-_)) :- atom(H),functor(H0,H,_), !.
+cf(H,(H0=>_)) :- atom(H),functor(H0,H,_).
 
 %defined such that call((\+ p),Y) is interpreted as \+ p(Y).
 \+(X,Y) :- \+ call(X,Y).
