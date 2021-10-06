830feb286e3bd27c659ddf8ad2d1c30dd59c9047
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-10-06 18:58:20 -0500

    document clear/0

--- tty.pl
+++ tty.pl
@@ -2,4 +2,5 @@ esc('\33\').
 csi(X) :- esc(Esc), atom_concat(Esc,'[',X).
 csi(C,R) :- esc(Esc), atom_concat(Esc,'[',X), atom_concat(X,C,R).
 
+clear/0 ?> 'clear the terminal screen'.
 clear :- write <-- csi('2J'), write <-- csi('H').
