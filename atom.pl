c82dcd0956e76008b36bd6395aa6b187f9e21d43
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-10-06 10:26:31 -0500

    update help directives in atom.pl to use new syntax; add tests

--- atom.pl
+++ atom.pl
@@ -1,7 +1,7 @@
-help(endswith/2, 'true iff Y ends with X').
-help(startswith/2, 'true iff Y starts with X').
-
+endswith/2 ?> 'true iff Y ends with X' @> endswith(pdf, 'test.pdf').
 endswith(E,A) :- atom_concat(_,E,A).
+
+startswith/2 ?> 'true iff Y starts with X' @> startswith(test, 'test.pdf').
 startswith(S,A) :- atom_concat(S,_,A).
 
 atom_resolve/2 ?> 'attempt to resolve X to an atom; if X is nonground, it is called as a unary predicate'
@@ -30,8 +30,12 @@ atom_resolve(X,Y) :- callable(X), call(X,Y), atom(Y).
 ++(X,Y,R) :- atom_resolve(X,X0), atom_resolve(Y,Y0), atom_concat(X0,Y0,R).
 X++Y :- X++Y <> (=).
 
-help((dot)/3, 'concatenate atoms X and Y with a \'.\' character').
-help((dot)/2, 'output the result of concatenating atoms X and Y with a \'.\' character').
+(dot)/3 ?> 'R is an atom containing the atoms X and Y joined by a \'.\' character'
+  @> dot(t,l, 't.l')
+  @> dot(test,pdf,'test.pdf')
+  @> dot(1,2,'1,2').
+
+(dot)/2 ?> 'Output the result of joining the atoms X and Y with a dot character'.
 
 dot(X,Y,R) :- R <- X++'.'++Y.
 X dot Y :- X dot Y <> (=).
