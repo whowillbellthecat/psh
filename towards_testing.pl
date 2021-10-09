301203caa7ad865b60ab33956ed5335e521b6c09
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-10-09 13:07:57 -0500

    extend spellcheck/1 with support for a custom psh_jargon dict file

--- towards_testing.pl
+++ towards_testing.pl
@@ -1,7 +1,6 @@
 % This file is for psh developement and does not get built with psh. It is intended to be consulted if working on psh (add to pshrc).
 % (A more roboust mechanism for its use may eventually be added).
 
-
 % crude but effective. lists undocumented and untested predicates. intended to be consulted if working on psh (add to pshrc).
 
 no_docs :-
@@ -11,12 +10,20 @@ no_tests :-
 	maplist(println) <-- columnize <-- sort <-- findall(P/N,
 			( psh_clause_line(P,N,_), (\+ psh_meta(P/N,_,_) ; \+ (psh_meta(P/N,_,L), L \= [])))) <> maplist(swap(write_to_atom)).
 
+
+% change this if the dictionary is installed at a different path
+dict_path(//usr/local/share/dict/psh_jargon).
+
 spellcheck :- spellcheck([]).
 
 spellcheck(R) :-
         findall(H, psh_meta(_/_,H,_),X),
         maplist(atom_codes,X,X0),
-        T <- cmd(spell,[],X0) &= spellcheck_,
+	Words <- dict_path <> atom_resolve,
+	(  file_exists(Words)
+	-> atom_concat('+',Words,Arg), Args = [Arg]
+	;  Args = [] ),
+        T <- cmd(spell,Args,X0) &= spellcheck_,
 	maplist(atom_codes,R,T).
 
 spellcheck_([X|Xs]) :- \+ (X #>= 65, X #=< 90), spellcheck_(Xs).
