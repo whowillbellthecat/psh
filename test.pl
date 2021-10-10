ab78850db4d2bcd1348a51bbb37d8f8c65929967
whowillbellthecat <everythingisbotnet@protonmail.com>
2021-10-10 14:11:37 -0500

    correct do_test docstring

--- test.pl
+++ test.pl
@@ -4,5 +4,5 @@ rt :- spawn(psh,['-c','do_tests.']).
 do_tests/0 ?> 'run all tests'.
 do_tests :- forall((psh_meta(C/N, _, T), T \= []), (write_to_atom(X,C/N), format('~30a\t', [X]), maplist(do_test,T), nl )).
 
-do_test/1 ?> 'run all tests for X'.
+do_test/1 ?> 'run test and write output passed on pass/fail'.
 do_test(T) :- call(T) -> write('.') ; nl, write(T), nl, fail.
